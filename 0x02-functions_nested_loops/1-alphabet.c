3c637af95b9d4f2a947ea9ffb4c2de308d8a8f12
Olulaw <bamidelelaw@gmail.com>
2022-10-19 11:23:08 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,12 @@
 /**
  * print_alphabet - print all alphabet in lowercase
 */
+
 void print_alphabet(void);
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter)
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c)
 
 	_putchar('\n')
 }
